24f7723614d151e364fe8da6f908001b4cb974d8
Christian <kpnguyen1013@gmail.com>
2020-11-23 04:28:01 -0600

    Complete view complete

--- nav.js
+++ nav.js
@@ -3,28 +3,38 @@ const navMenu = document.querySelector('.nav-menu');
 const mask = document.querySelector('.mask');
 const body = document.querySelector('body');
 
+//GSAP Code Here
+
 function toggleScrollable() {
     body.classList.toggle('unscrollable');
 }
 
 function toggleNavMenu() {
-
+    navMenu.classList.toggle('nav-menu-active');
 }
 
 function toggleNavImage() {
-    const src = `./${navControl.src}`;
-    console.log(src)
-    if(src === './images/icon-hamburger.svg') {
-        navControl.src = './images/icon-close.svg';
+    const src = navControl.src;
+    const splitSrc = src.split('/');
+    const idxLast = splitSrc.length - 1;
+    if (splitSrc[idxLast] === 'icon-hamburger.svg') {
+        splitSrc[idxLast] = 'icon-close.svg';
     } else {
-        navControl.src = './images/icon-hamburger.svg';
+        splitSrc[idxLast] = 'icon-hamburger.svg';
     }
+    navControl.src = splitSrc.join('/');
+}
+
+function toggleMask() {
+    mask.classList.toggle('mask-active');
 }
 
 function menuClick() {
     toggleNavMenu();
     toggleNavImage();
     toggleScrollable();
+    toggleMask();
 }
 
-navControl.addEventListener('click', menuClick)
+navControl.addEventListener('click', menuClick);
+mask.addEventListener('click', menuClick);
